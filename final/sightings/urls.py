47a4dad65982254ae096da367b17417a5c6e32aa
“helenmonster” <56941185+helenmonster@users.noreply.github.com>
2019-12-08 02:42:21 +0000

    second try on Dec 7th

---
+++ final/sightings/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+        path('map/',views.map, name = 'map'),
+        path('sightings/', views.sightings, name = 'sightings'),
+        path('sightings/<squirrel_id>/',views.id, name = 'id'),
+        path('sightings/add/',views.add, name = 'add'),
+        path('sightings/stats/', views.stats, name = 'stats'),
+]
+
